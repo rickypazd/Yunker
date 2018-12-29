a26477e7af87cad282583ec47b8f065b0b32dc63
edson <edson.nur@gmail.com>
2018-12-29 00:16:06 -0400

    add

--- web/js/Vehiculo/vehiculo.js
+++ web/js/Vehiculo/vehiculo.js
@@ -7,6 +7,7 @@
 var url = "repuestosController";
 $(document).ready(function () {
     cargar_marcas();
+    cargar_version_auto();
 });
 
 function cargar_marcas() {
@@ -31,11 +32,14 @@ function cargar_marcas() {
         }
     });
 }
+
+
 function cargar_lista_marca(arr){
     $.each(arr,function(i,obj){
         cargar_iten_marca(obj); 
     });
 }
+
 function cargar_iten_marca(obj) {
     var url_foto= "img/Sin_imagen.png";
     if(obj.url_foto.length>0){
@@ -51,3 +55,31 @@ function cargar_iten_marca(obj) {
 function seleccionar_marca(obj){
     alert(obj.id);
 }
+
+function cargar_version_auto() {
+    var html = "";
+    //mostrar_progress();
+    $.post(url, {TokenAcceso: "servi12sis3", evento: "getAll_rep_auto_version"}, function (resp) {
+        //  cerrar_progress();
+        if (resp != null) {
+            var obj = $.parseJSON(resp);
+            if (obj.estado != "1") {
+                alert(obj.mensaje);
+            } else {
+                var arr = $.parseJSON(obj.resp);
+                $.each(arr, function (i, obj) {
+                    html += "<li class='list-group-item' data-obj='"+JSON.stringify(obj)+"' onclick='agregar_version(this);'>" + obj.nombre + "</li>";
+                });
+                $("#lista_version").html(html);
+            }
+        }
+    });
+}
+    function agregar_version(iten){
+       
+        $(iten).remove();
+        $("#lista_version_agregadas").add(iten);
+    }
+
+
+
